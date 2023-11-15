f15a642145b903bd516dd7d91c17be4e34144fd2
APF2000 <arthurpfonseca@gmail.com>
2023-11-15 13:05:19 -0300

    generate nicervelocity graphic

--- dataset_car/app_data_parser.py
+++ dataset_car/app_data_parser.py
@@ -8,13 +8,13 @@ def get_data_from_app():
 
 	# ex: Wed Jul 26 22:15:49 GMT+02:00 2023
 	original_date_format = "%a %b %d %H:%M:%S GMT%z %Y"
-	desired_date_format = "%Y-%m-%d %H:%M:%S"
+	# desired_date_format = "%Y-%m-%d %H:%M:%S"
 
 	param_name_to_df = {}
 
 	engine_data = open(engine_data_path, "r").read().strip()
 	for data_entry in engine_data.split("\n"):
-		data_entry = data_entry.replace("NODATA", "")
+		data_entry = data_entry.replace("NODATA", "0")
 
 		date = data_entry[:34]
 		info_list = json.loads(data_entry[35:])
@@ -26,7 +26,8 @@ def get_data_from_app():
 		# print("info_list: ", info_list)
 
 		data_obj = datetime.strptime(date, original_date_format)
-		timestamp = data_obj.timestamp() * 1000
+		timestamp = data_obj
+		# timestamp = data_obj.timestamp() * 1000
 		# print(timestamp)
 
 		create_default_df = (lambda : pd.DataFrame(columns=["timestamp", param_name]))
@@ -34,5 +35,9 @@ def get_data_from_app():
 
 		param_df.loc[len(param_df.index)] = [timestamp, param_value]
 		param_name_to_df[param_name] = param_df
+
+	speed_series = param_name_to_df["SPEED"]["SPEED"]
+	convert_vel_to_float = lambda x : float(x.replace("km/h", ""))
+	param_name_to_df["SPEED"]["SPEED"] = speed_series.apply(convert_vel_to_float)
 
 	return param_name_to_df
