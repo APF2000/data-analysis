8f6032f11f51168982a661af684dccaf481e01ca
APF2000 <arthurpfonseca@gmail.com>
2023-11-20 12:13:16 -0300

    add new car data to database

--- dataset_car/ride_parser.py
+++ dataset_car/ride_parser.py
@@ -24,6 +24,46 @@ class RealRideParser():
 		self.root_dir = root_dir
 		self.gps_df = self.create_gps_df()
 		self.accelerometer_df = self.create_accelerometer_df()
+		self.obd_data = self.get_data_from_app()
+
+	def get_data_from_app(self):
+		engine_data_path = os.path.join(self.root_dir, "DELETEME.txt")
+
+		# ex: Wed Jul 26 22:15:49 GMT+02:00 2023
+		original_date_format = "%a %b %d %H:%M:%S %Z%z %Y"
+		# desired_date_format = "%Y-%m-%d %H:%M:%S"
+
+		param_name_to_df = {}
+
+		engine_data = open(engine_data_path, "r").read().strip()
+		for data_entry in engine_data.split("\n"):
+			data_entry = data_entry.replace("NODATA", "0")
+
+			date = data_entry[:34]
+			info_list = json.loads(data_entry[35:])
+
+			param_name = info_list[0]
+			param_value = info_list[2]
+
+			# print("date: ", date)
+			# print("info_list: ", info_list)
+
+			data_obj = datetime.strptime(date, original_date_format)
+			timestamp = data_obj
+			timestamp = data_obj.timestamp() # * 1000
+			# print(timestamp)
+
+			create_default_df = (lambda : pd.DataFrame(columns=["timestamp", param_name]))
+			param_df = param_name_to_df.get(param_name, create_default_df())
+
+			param_df.loc[len(param_df.index)] = [timestamp, param_value]
+			param_name_to_df[param_name] = param_df
+
+		speed_series = param_name_to_df["SPEED"]["SPEED"]
+		convert_vel_to_float = lambda x : float(x.replace("km/h", ""))
+		param_name_to_df["SPEED"]["SPEED"] = speed_series.apply(convert_vel_to_float)
+
+		return param_name_to_df
 
 	def create_velocity_from_gps_df(self):
 		gps_df = self.gps_df #.drop_duplicates(subset=["timestamp"])
